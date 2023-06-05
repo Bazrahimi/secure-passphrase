cda6162965d6b71f1dbcf97c128e67433111af8d
Baz Rahimi <bazrahimi@hotmail.com>
2023-06-06 07:58:36 +1000

    add GeeratePassword function and promts

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  let length = parseInt(prompt(` Enter the length of your desired password:
+  Password Length betweeen 8 - 128 characters.` ));
+  let includeUpperCase = confirm('Include Uppercase Letters?');
+  let includeLowerCase = confirm('Include Lowercase letters?');
+  let includeNumbers = confirm('Include Numbers?');
+  let includeSpecialChars = confirm('Include Special Characters?');
+
+
+
+
+
+  return 'generated password will go in here';
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +30,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//pseudo coding
+/*
+1. when we click on generate button
+2. a series of prompt will be presented for password criteria.
+3. when prompt for the length of password, than we must choose the the lenght of passord between 8 - 128         characters.
+4.than confirm wether or not to include lowercase, uppercase, numeric , and /or special characters.
+5. at one character type should be selected.
+6. than a password is generted that match the criteria.
+than the password is either displayed in alert or written the page.
+
+
+
+
+*/
